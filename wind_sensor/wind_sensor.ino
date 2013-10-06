9296f034daf0f2c3926c007bded05758fe4a34f9
Zoe Kay <zkay@newrelic.com>
2013-10-05 18:41:26 -0700

    initial file

---
+++ wind_sensor/wind_sensor.ino
@@ -0,0 +1,54 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+#include <Servo.h> 
+// named constants for the switch and motor pins
+const int servoPin =  9; // the number of the motor pin
+const int motorPin = 10;
+const int led = 13;
+const int windSensor = 1;
+int val;
+
+
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object 
+  pinMode(led, OUTPUT);
+  pinMode(motorPin,   OUTPUT);  
+  // digitalWrite(motorPin, HIGH);
+  pinMode(windSensor, INPUT);
+  Serial.begin(9600);
+  Serial.println("Hi there");
+} 
+ 
+ 
+void loop()
+{ 
+  
+  val = analogRead(windSensor);
+  val = map(val, 0, 1023, 0, 179);
+  Serial.println(val);
+  myservo.write(val);
+  delay(1000);
+//  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+//  {                                  // in steps of 1 degree 
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(30);
+//
+//  } 
+//  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+//  {                                
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'  
+//    delay(30);
+//  } 
+
+  //digitalWrite(led, HIGH);
+  //digitalWrite(motorPin, HIGH); 
+  //delay(1000);
+  //digitalWrite(motorPin, LOW);
+} 
